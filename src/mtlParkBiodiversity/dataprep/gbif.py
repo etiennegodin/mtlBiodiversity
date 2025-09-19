e0ba799cff73f40e8a2f010e1427488bf5a008c8
Etienne <manateetiti@gmail.com>
2025-09-18 22:19:54 -0400

    gbfi cleanup before grid

--- src/mtlParkBiodiversity/dataprep/gbif.py
+++ src/mtlParkBiodiversity/dataprep/gbif.py
@@ -40,8 +40,6 @@ def ask_user_grid_file(GEOSPATIAL_PATH):
     return grid_file, nbhood_file, park_file
 
 
-    
-
 def convert_gbif_csv(input_path, output_path, force = False, test = False, limit = None):
 
     if test:
@@ -51,14 +49,6 @@ def convert_gbif_csv(input_path, output_path, force = False, test = False, limit
     else: 
         print(f'Converting to {input_path} to {output_path} file ')
         duckdb.query(f"COPY (SELECT * FROM '{input_path}') TO '{output_path}' (FORMAT PARQUET)")
-  
-
-def preview_gbif_data(con, table, limit = 100):
-    print('-'*25, 'Preview', '-'*25)
-    df = con.execute(f"SELECT * FROM {table} LIMIT {limit}").df()
-    print(df.head(limit))
-    print(df.columns)
-    print('/'*50)
 
 def prep_park_sql(park_file, alias = 'p'):
 
@@ -79,41 +69,6 @@ def prep_park_sql(park_file, alias = 'p'):
 
     return sql_query
 
-def check_crs(file, debug = False):
-    if debug:
-        print(file.suffix)
-    if file.suffix == ".shp":
-        try:
-            x = gpd.read_file(file)
-            crs = x.crs
-            print(crs)
-            return crs
-
-        except Exception as e:
-            print(f'Error reading {file} : {e}')
-            return False
-    elif file.suffix == '.parquet':
-        try:
-            crs = x.crs
-            print(crs)
-            return crs
-
-        except Exception as e:
-            print(f'Error reading {file} : {e}')
-            return False
-    else:
-        print(f'Error reading {file}')
-        return False
-
-    
-def get_gbif_data_crs(gbif_occurence_raw_file, lat, long, limit = 10, ):
-    from shapely.geometry import Point
-
-    df = pd.read_csv(gbif_occurence_raw_file, nrows = limit, delimiter = '\t')
-    gdf = gpd.GeoDataFrame(df, geometry=[Point(xy) for xy in zip(df["decimalLongitude"], df["decimalLatitude"])] , crs = 4326 )
-    print(gdf.head())
-    print(gdf.crs)
-    pass
 
 def convert_lat_long_to_point(con, table):
     #Gbif occ to geometry 
@@ -137,7 +92,7 @@ def spatial_join(gbif_occurence_db_file, park_file, output_file_path = None, tes
     #Load parks boundary file 
     con.execute(f"CREATE OR REPLACE TABLE parks AS SELECT * FROM ST_Read('{park_file}')")
     park_fields_sql = prep_park_sql(park_file)
-    #preview_gbif_data(con, "parks")
+
     print('Creating gbif table...')
     #Load gbif data
     if test:
@@ -237,7 +192,6 @@ def prep_gbif(force = False, test = False, colab = False, limit = None):
 
     x = ask_user_grid_file(GEOSPATIAL_PATH)
 
-    return
 
     # Create output directory if not existing
     park_file =  [f for f in GEOSPATIAL_PATH.rglob("*.shp")][0]  # Assuming there's only one .shp file for the park data
@@ -259,10 +213,10 @@ def prep_gbif(force = False, test = False, colab = False, limit = None):
             print("Convert_gbif_csv already done, skipping")
 
     if (output_file_path.exists() and force) or (not output_file_path.exists()):
-        pass
         spatial_join(gbif_occurence_db_file,park_file, output_file_path = output_file_path, test = test, limit = limit, colab = colab)
     else:
         print("Spatial Join already done, skipping")
     
 if __name__ == "__main__":
+
     prep_gbif(force= True)
