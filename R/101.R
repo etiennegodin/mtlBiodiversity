87fdb585f421c22482c5f106a377e9f7867071fd
Etienne <manateetiti@gmail.com>
2025-09-24 22:50:48 -0400

    r 101

--- R/101.R
+++ R/101.R
@@ -1,6 +1,25 @@
 library(DBI)
 library(duckdb)
+library(tidyverse)
 
 con <- dbConnect(duckdb::duckdb(), dbdir = "C:/Users/manat/Documents/Projects/mtlBiodiversity/data/db/mtlbio.duckdb", read_only = TRUE)
 dbListTables(con)
 df <- dbReadTable(con, "quartiers_sjoin")   # read into R
+
+view(df)
+glimpse((df))
+
+?unique
+
+df %>% 
+  group_by(qrt_id) %>% 
+  summarise(
+    species_richness = n_distinct(species),
+    genus_richness = n_distinct(genus),
+    family_richness = n_distinct(family),
+    order_richness = n_distinct(order),
+    class_richness = n_distinct(class)
+    
+    
+  )
+
