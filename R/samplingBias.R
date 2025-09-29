971cc28f9bb18d8f1ab4c98c6024f963e90fc1df
Etienne <manateetiti@gmail.com>
2025-09-29 18:26:29 -0400

    wip

--- R/samplingBias.R
+++ R/samplingBias.R
@@ -28,22 +28,65 @@ users <- users$identifiedBy
 View(users)
 
 
+cluster_stats <- function(df, coords_utm)
+{
+  # ---------------------------------------------------------
+  # 3. Summarise cluster stats
+  # ---------------------------------------------------------
+  df$x <- coords_utm[,1]
+  df$y <- coords_utm[,2]
+  
+  cluster_stats <- df %>%
+    group_by(cluster) %>%
+    summarise(
+      n_obs = n(),
+      n_species = n_distinct(species),
+      n_users = n_distinct(identifiedBy),
+      # spread = mean distance to centroid
+      spread_mean_m = {
+        centroid <- colMeans(cbind(x, y))
+        mean(sqrt((x - centroid[1])^2 + (y - centroid[2])^2))
+      },
+      spread_max_m = {
+        centroid <- colMeans(cbind(x, y))
+        max(sqrt((x - centroid[1])^2 + (y - centroid[2])^2))
+      }
+    )
+  
+  print(cluster_stats)
+  
+  
+}
+
+db_scan_func <- function(df)
+{
+  
+  coords <- df[, c('decimalLongitude', 'decimalLatitude')]
+  df_sf <- st_as_sf(df, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+  df_utm <- st_transform(df_sf, 32618)  # pick your UTM zone
+  coords_utm <- st_coordinates(df_utm)
+  
+  db <- dbscan(coords_utm, eps = 50, minPts = 3)
+  #db_h <- hdbscan(coords_utm, minPts = 5)
+  df$cluster <- db$cluster
+  View(df)
+  
+  cluster_stats(df, coords_utm)
+  
+  
+}
 
 
 for (u in users)
 {
   print(paste("Hello", u))
-  next
   df_temp <- filter(df, identifiedBy == u)
-  #?distm
-  coords <- df_temp[, c('decimalLongitude', 'decimalLatitude')]
-  df_sf <- st_as_sf(df_temp, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
-  df_utm <- st_transform(df_sf, 32618)  # pick your UTM zone
-  coords_utm <- st_coordinates(df_utm)
-
-  #db <- dbscan(coords_utm, eps = 50, minPts = 5)
-  db_h <- hdbscan(coords_utm, minPts = 5)
-  df_temp$cluster <- db_h$cluster
+  
+  db_scan_func(df_temp)
+  break
+  
+  
+  
 }
 
 
